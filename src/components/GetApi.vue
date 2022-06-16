ee420a77fe39efa9d64fbf60e2863041c1b4a74a
Lucas Paulino Marra <lucas.marra.terceirizado@tre-go.jus.br>
2022-06-16 18:24:20 -0300

    starting repo

---
+++ src/components/GetApi.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+    <div class="px-4 py-5 sm:px-6 text-center">
+      <h3 class="text-lg leading-6 font-medium text-gray-900">Benefícios concedidos no Brasil</h3>
+
+    </div>
+    <div class="border-t border-gray-200">
+      <dl>
+        <Input/>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<script>
+import Input from './components/Input';
+export default {
+  name: "GetApi"
+}
+</script>
+
+<style scoped>
+
+
+</style>
